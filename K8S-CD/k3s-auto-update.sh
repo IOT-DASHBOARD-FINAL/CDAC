f60ec8f92ac7a17a831e6fb498033d50c215d3ea
THARANIKA-R <tharanikaramasamy6@gmail.com>
2025-06-09 19:42:10 +0530

    Added changes

--- K8S-CD/k3s-auto-update.sh
+++ K8S-CD/k3s-auto-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Checking for new image at $(date)..."
 
-kubectl set image deployment/iot-atem-frontend frontend-container=kaviraj2003/iot-frontend:latest --record
+kubectl set image deployment/iot-atem-frontend frontend-container=tharanika/iot-frontend:latest --record
 kubectl rollout restart deployment/iot-application-frontend
 
-kubectl set image deployment/iot-application-backend backend-container=kaviraj2003/iot-backend:latest --record
+kubectl set image deployment/iot-application-backend backend-container=tharanika/iot-backend:latest --record
 kubectl rollout restart deployment/iot-application-backend
